10e4f188cbb5fd5e01be241e4eb70991c9a07ff8
HYK <junhyeok001010@gmail.com>
2025-12-31 00:04:11 +0900

    1.3.1 클래스의 분리

--- src/main/java/com/hyk/user/dao/SimpleConnectionMaker.java
+++ src/main/java/com/hyk/user/dao/SimpleConnectionMaker.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DUserDao extends UserDao {
+public class SimpleConnectionMaker {
 
-  public Connection getConnection() throws ClassNotFoundException, SQLException {
+  public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook", "root", "password1234");
     return c;
