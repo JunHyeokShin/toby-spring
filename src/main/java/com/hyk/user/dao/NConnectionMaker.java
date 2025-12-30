b93d29258ab5850c5f26ae6241d2ee95253d2c35
HYK <junhyeok001010@gmail.com>
2025-12-31 00:06:42 +0900

    1.3.2 인터페이스의 도입

--- src/main/java/com/hyk/user/dao/NConnectionMaker.java
+++ src/main/java/com/hyk/user/dao/NConnectionMaker.java
@@ -4,9 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
+public class NConnectionMaker implements ConnectionMaker {
 
-  public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+  public Connection makeConnection() throws ClassNotFoundException, SQLException {
     Class.forName("com.mysql.cj.jdbc.Driver");
     Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/springbook", "root", "password1234");
     return c;
