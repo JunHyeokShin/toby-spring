92a882738e9f338d0f8fe930629fc799a4d3d34b
HYK <junhyeok001010@gmail.com>
2025-12-31 03:42:53 +0900

    2.4.2 DI와 테스트(테스트 코드에 의한 DI)

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,17 @@
+CREATE DATABASE IF NOT EXISTS springbook;
+USE springbook;
+
+CREATE TABLE users
+(
+    id       VARCHAR(10) PRIMARY KEY,
+    name     VARCHAR(20) NOT NULL,
+    password VARCHAR(10) NOT NULL
+);
+
+-- testdb 데이터베이스 생성 및 테이블 복사
+CREATE DATABASE IF NOT EXISTS testdb;
+USE testdb;
+
 CREATE TABLE users
 (
     id       VARCHAR(10) PRIMARY KEY,
