612f093b46429abdb35b86809d016905fcb1085e
HYK <junhyeok001010@gmail.com>
2026-01-02 00:31:48 +0900

    3.4.1 JdbcContext의 분리

--- src/test/java/com/hyk/user/dao/UserDaoTest.java
+++ src/test/java/com/hyk/user/dao/UserDaoTest.java
@@ -3,16 +3,24 @@ package com.hyk.user.dao;
 import com.hyk.user.domain.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.sql.DataSource;
 import java.sql.SQLException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
 public class UserDaoTest {
+
+  @Autowired
+  private ApplicationContext context;
 
   private UserDao dao;
 
@@ -22,11 +30,7 @@ public class UserDaoTest {
 
   @Before
   public void setUp() {
-    dao = new UserDao();
-    DataSource dataSource = new SingleConnectionDataSource(
-        "jdbc:mysql://localhost:3306/testdb", "root", "password1234", true
-    );
-    dao.setDataSource(dataSource);
+    this.dao = this.context.getBean("userDao", UserDao.class);
 
     this.user1 = new User("gyumee", "박성철", "springno1");
     this.user2 = new User("leegw700", "이길원", "springno2");
