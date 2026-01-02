5ba270bcf68b8f8fc02a745859c4ae9b21088d90
HYK <junhyeok001010@gmail.com>
2026-01-02 22:11:13 +0900

    3.6.4 query()

--- src/main/java/com/hyk/user/dao/UserDao.java
+++ src/main/java/com/hyk/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
 
@@ -46,6 +47,21 @@ public class UserDao {
   public int getCount() {
     // JdbcTemplate.queryForInt() 메소드는 4.2.0 버전 이후로 제거되었음
     return this.jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+  }
+
+  public List<User> getAll() {
+    return this.jdbcTemplate.query(
+        "SELECT * FROM users ORDER BY id",
+        new RowMapper<User>() {
+          public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+            return user;
+          }
+        }
+    );
   }
 
 }
