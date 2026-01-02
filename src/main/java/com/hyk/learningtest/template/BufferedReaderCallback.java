dfc29c78443071446ac21623aa39ca90e1b10c60
HYK <junhyeok001010@gmail.com>
2026-01-02 18:06:57 +0900

    3.5.3 템플릿/콜백의 응용(중복의 제거와 템플릿/콜백 설계)

---
+++ src/main/java/com/hyk/learningtest/template/BufferedReaderCallback.java
@@ -0,0 +1,10 @@
+package com.hyk.learningtest.template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public interface BufferedReaderCallback {
+
+  Integer doSomethingWithReader(BufferedReader br) throws IOException;
+
+}
