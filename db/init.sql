afc1201cd72bf1e9736216785ada150cd0b0dc08
HYK <junhyeok001010@gmail.com>
2026-01-04 18:41:48 +0900

    5.1.1 필드 추가

--- db/init.sql
+++ db/init.sql
@@ -3,9 +3,12 @@ USE springbook;
 
 CREATE TABLE users
 (
-    id       VARCHAR(10) PRIMARY KEY,
-    name     VARCHAR(20) NOT NULL,
-    password VARCHAR(10) NOT NULL
+    id        VARCHAR(10) PRIMARY KEY,
+    name      VARCHAR(20) NOT NULL,
+    password  VARCHAR(10) NOT NULL,
+    level     TINYINT     NOT NULL,
+    login     INT         NOT NULL,
+    recommend INT         NOT NULL
 );
 
 -- testdb 데이터베이스 생성 및 테이블 복사
@@ -14,7 +17,10 @@ USE testdb;
 
 CREATE TABLE users
 (
-    id       VARCHAR(10) PRIMARY KEY,
-    name     VARCHAR(20) NOT NULL,
-    password VARCHAR(10) NOT NULL
+    id        VARCHAR(10) PRIMARY KEY,
+    name      VARCHAR(20) NOT NULL,
+    password  VARCHAR(10) NOT NULL,
+    level     TINYINT     NOT NULL,
+    login     INT         NOT NULL,
+    recommend INT         NOT NULL
 );
--- src/main/java/com/hyk/user/dao/UserDaoJdbc.java
+++ src/main/java/com/hyk/user/dao/UserDaoJdbc.java
@@ -1,5 +1,6 @@
 package com.hyk.user.dao;
 
+import com.hyk.user.domain.Level;
 import com.hyk.user.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -18,6 +19,9 @@ public class UserDaoJdbc implements UserDao {
       user.setId(rs.getString("id"));
       user.setName(rs.getString("name"));
       user.setPassword(rs.getString("password"));
+      user.setLevel(Level.valueOf(rs.getInt("level")));
+      user.setLogin(rs.getInt("login"));
+      user.setRecommend(rs.getInt("recommend"));
       return user;
     }
   };
@@ -27,8 +31,11 @@ public class UserDaoJdbc implements UserDao {
   }
 
   public void add(User user) {
-    this.jdbcTemplate.update("INSERT INTO users(id, name, password) VALUES(?, ?, ?)",
-        user.getId(), user.getName(), user.getPassword());
+    this.jdbcTemplate.update(
+        "INSERT INTO users(id, name, password, level, login, recommend) VALUES(?, ?, ?, ?, ? ,?)",
+        user.getId(), user.getName(), user.getPassword(),
+        user.getLevel().intValue(), user.getLogin(), user.getRecommend()
+    );
   }
 
   public User get(String id) {
