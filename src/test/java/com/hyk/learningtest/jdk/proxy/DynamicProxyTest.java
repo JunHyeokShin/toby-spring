7a3d359f00da3873971d18802dddfc3d9f2819c4
HYK <junhyeok001010@gmail.com>
2026-01-07 00:54:55 +0900

    6.3.2 다이내믹 프록시(다이내믹 프록시 적용)

--- src/test/java/com/hyk/learningtest/jdk/proxy/DynamicProxyTest.java
+++ src/test/java/com/hyk/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -2,6 +2,8 @@ package com.hyk.learningtest.jdk.proxy;
 
 import org.junit.Test;
 
+import java.lang.reflect.Proxy;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -14,7 +16,11 @@ public class DynamicProxyTest {
     assertThat(hello.sayHi("Toby"), is("Hi Toby"));
     assertThat(hello.sayThankYou("Toby"), is("Thank You Toby"));
 
-    Hello proxiedHello = new HelloUppercase(new HelloTarget());
+    Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+        getClass().getClassLoader(),
+        new Class[]{Hello.class},
+        new UppercaseHandler(new HelloTarget())
+    );
     assertThat(proxiedHello.sayHello("Toby"), is("HELLO TOBY"));
     assertThat(proxiedHello.sayHi("Toby"), is("HI TOBY"));
     assertThat(proxiedHello.sayThankYou("Toby"), is("THANK YOU TOBY"));
