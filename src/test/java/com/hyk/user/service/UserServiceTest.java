4aea44601977699307b63a660e2f6fd35508ebc5
HYK <junhyeok001010@gmail.com>
2026-01-06 19:28:25 +0900

    5.4.4 테스트 대역

--- src/test/java/com/hyk/user/service/UserServiceTest.java
+++ src/test/java/com/hyk/user/service/UserServiceTest.java
@@ -7,11 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,9 +52,13 @@ public class UserServiceTest {
   }
 
   @Test
+  @DirtiesContext
   public void upgradeLevels() {
     userDao.deleteAll();
     for (User user : users) userDao.add(user);
+
+    MockMailSender mockMailSender = new MockMailSender();
+    userService.setMailSender(mockMailSender);
 
     userService.upgradeLevels();
 
@@ -59,6 +67,11 @@ public class UserServiceTest {
     checkLevelUpgraded(users.get(2), false);
     checkLevelUpgraded(users.get(3), true);
     checkLevelUpgraded(users.get(4), false);
+
+    List<String> requests = mockMailSender.getRequests();
+    assertThat(requests.size(), is(2));
+    assertThat(requests.get(0), is(users.get(1).getEmail()));
+    assertThat(requests.get(1), is(users.get(3).getEmail()));
   }
 
   @Test
@@ -105,6 +118,25 @@ public class UserServiceTest {
     } else {
       assertThat(userUpdate.getLevel(), is(user.getLevel()));
     }
+  }
+
+  static class MockMailSender implements MailSender {
+
+    private List<String> requests = new ArrayList<String>();
+
+    public List<String> getRequests() {
+      return requests;
+    }
+
+    @Override
+    public void send(SimpleMailMessage simpleMessage) throws MailException {
+      requests.add(simpleMessage.getTo()[0]);
+    }
+
+    @Override
+    public void send(SimpleMailMessage... simpleMessages) throws MailException {
+    }
+
   }
 
   static class TestUserService extends UserService {
