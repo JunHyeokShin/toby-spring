23a9f16ad604770802e457d6f202b0b1eaf48d88
HYK <junhyeok001010@gmail.com>
2026-01-08 02:40:28 +0900

    6.5.3 포인트컷 표현식을 이용한 포인트컷

--- src/test/java/com/hyk/user/service/UserServiceTest.java
+++ src/test/java/com/hyk/user/service/UserServiceTest.java
@@ -214,7 +214,7 @@ public class UserServiceTest {
 
   }
 
-  static class TestUserServiceImpl extends UserServiceImpl {
+  static class TestUserService extends UserServiceImpl {
 
     private String id = "madnite1";
 
