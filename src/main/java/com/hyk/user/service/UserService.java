c4721616e881946350ddf074b3ef3ba7eafadc79
HYK <junhyeok001010@gmail.com>
2026-01-08 22:23:00 +0900

    6.7.2 트랜잭션 애노테이션 적용

--- src/main/java/com/hyk/user/service/UserService.java
+++ src/main/java/com/hyk/user/service/UserService.java
@@ -1,15 +1,19 @@
 package com.hyk.user.service;
 
 import com.hyk.user.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
 
   void add(User user);
 
+  @Transactional(readOnly = true)
   User get(String id);
 
+  @Transactional(readOnly = true)
   List<User> getAll();
 
   void update(User user);
