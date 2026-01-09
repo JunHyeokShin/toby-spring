fb369afc18bfa1f70e3355c78efd69df37ca75cc
HYK <junhyeok001010@gmail.com>
2026-01-09 18:40:24 +0900

    7.1.2 SQL 제공 서비스

---
+++ src/main/java/com/hyk/user/sqlservice/SqlRetrievalFailureException.java
@@ -0,0 +1,13 @@
+package com.hyk.user.sqlservice;
+
+public class SqlRetrievalFailureException extends RuntimeException {
+
+  public SqlRetrievalFailureException(String message) {
+    super(message);
+  }
+
+  public SqlRetrievalFailureException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+}
