8c13877dbab3ab9780477a306683f6863c2811dc
HYK <junhyeok001010@gmail.com>
2026-01-13 01:45:05 +0900

    7.2.1 XML 파일 매핑

---
+++ src/main/java/com/hyk/user/sqlservice/jaxb/package-info.java
@@ -0,0 +1,9 @@
+//
+// �� ������ JAXB(JavaTM Architecture for XML Binding) ���� ���� 2.2.8-b130911.1802 ������ ���� �����Ǿ����ϴ�.
+// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>�� �����Ͻʽÿ�.
+// �� ������ �����ϸ� �ҽ� ��Ű���� ���������� �� ���� ������ �սǵ˴ϴ�.
+// ���� ��¥: 2026.01.12 �ð� 12:41:52 AM KST
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.epril.com/sqlmap", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.hyk.user.sqlservice.jaxb;
