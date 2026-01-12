cf8d337b1d4a8025243a85fa0b4c29e4a0222b7c
HYK <junhyeok001010@gmail.com>
2026-01-13 01:46:57 +0900

    7.2.3 빈의 초기화 작업

--- src/main/java/com/hyk/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/hyk/user/sqlservice/XmlSqlService.java
@@ -3,6 +3,7 @@ package com.hyk.user.sqlservice;
 import com.hyk.user.dao.UserDao;
 import com.hyk.user.sqlservice.jaxb.SqlType;
 import com.hyk.user.sqlservice.jaxb.Sqlmap;
+import jakarta.annotation.PostConstruct;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
@@ -14,13 +15,19 @@ import java.util.Map;
 public class XmlSqlService implements SqlService {
 
   private Map<String, String> sqlMap = new HashMap<>();
+  private String sqlmapFile;
 
-  public XmlSqlService() {
+  public void setSqlmapFile(String sqlmapFile) {
+    this.sqlmapFile = sqlmapFile;
+  }
+
+  @PostConstruct
+  public void loadSql() {
     String contextPath = Sqlmap.class.getPackage().getName();
     try {
       JAXBContext context = JAXBContext.newInstance(contextPath);
       Unmarshaller unmarshaller = context.createUnmarshaller();
-      InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+      InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
       Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
       for (SqlType sql : sqlmap.getSql()) {
